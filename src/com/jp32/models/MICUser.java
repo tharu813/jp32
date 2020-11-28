d2213b76915c1a98e9caa2568d7f6c25e8e51722
tharu813 <kav.ds8@gmail.com>
2020-11-29 04:22:34 +0530

    Added some user classes

---
+++ src/com/jp32/models/MICUser.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jp32.models;
+
+/**
+ *
+ * @author THARUSHI
+ */
+public class MICUser extends AdminMemberUser {
+    
+    public MICUser(String userid, String username, String password, String firstName, String lastName) {
+        super(userid, username, password, firstName, lastName);
+    }
+    
+}
